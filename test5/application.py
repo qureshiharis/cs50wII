61d33f2a55ffbb6f404d13735c36327e5948caaf
Your Name <haris.qureshi96@gmail.comgit config --global user.name qureshiharisgit add hello.htmlgit commit -m Added>
2019-06-28 13:37:50 +0500

    jinja syntax in html

---
+++ test5/application.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    names = ["Jane", "John", "Gustavo"]
+    return render_template("index.html", names=names)
